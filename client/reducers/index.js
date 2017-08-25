56ce485a5bc6988744ac8519ee3f5d7a8920b395
Yevgeniy Valeyev <webmaster.vea@gmail.com>
2017-08-25 11:47:57 +0200

    Fixed restoring of state after browser refresh

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import byId, * as fromById from './collection-by-id';
 import ids from './ids-list';
 import isFetching from './fetching';
+import isLoaded from './stocks-loaded';
 import errorMessage from './error-message';
 
 const stocks = combineReducers({
   byId,
   ids,
   isFetching,
+  isLoaded,
   errorMessage,
 });
 
@@ -17,8 +19,12 @@ export const getStocks = (state) => {
   return state.ids.map((id) => fromById.getStock(state.byId, id));
 };
 
-export const getStockById = (state, id) => fromById.getStock(state.byId, id);
+export const getStockById = (state, id) => {
+  return fromById.getStock(state.byId, id);
+};
 
 export const getIsFetching = (state) => state.isFetching;
+
+export const getIsLoaded = (state) => state.isLoaded;
 
 export const getErrorMessage = (state) => state.errorMessage;
