b39244f9bd31300f862aca8c67c72a5ad1348636
Yevgeniy Valeyev <yevgeniyvaleyev@users.noreply.github.com>
2017-11-02 12:42:59 +0100

    Update 06-handle-client-route.js

--- server/middlewares/06-handle-client-route.js
+++ server/middlewares/06-handle-client-route.js
@@ -1,4 +1,4 @@
-const send = require('koa-send');
+// const send = require('koa-send');
 
 /**
  * Support for clint routes when user reloads a page being
@@ -9,9 +9,9 @@ const send = require('koa-send');
  */
 module.exports = async (ctx, next) => {
   await next();
-  const status = ctx.status || 404;
-  if (status === 404) {
-    await send(ctx, `${__dirname}/public/index.html`);
-  }
+//   const status = ctx.status || 404;
+//   if (status === 404) {
+//     await send(ctx, `${__dirname}/public/index.html`);
+//   }
 };
 
